01bc57399761ad2b4c4159ff1768ad389a6770fd
clf94-dev <clf94@hotmail.es>
2020-09-15 12:23:11 +0200

    added style to top section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,23 +3,26 @@ import {Grid} from '@material-ui/core/';
 import TopImg from './images/pattern-bg.png'
 import Arrow from './images/icon-arrow.svg'
 
+import './styles/App.css'
+
 function App() {
     return (
         <div>
-            <Grid container direction='column'>
-                <img src={TopImg} alt="top background"/>
+            <Grid container direction='column' className='top-section'>
+                <img src={TopImg} alt="top background" className='bg-img'/>
+                <Grid container className='top-section-cont'>
+                    <h1>IP Address Tracker</h1>
 
-                <h1>IP Address Tracker</h1>
-
-                <Grid container direction='row'>
-                    <Grid item xs={10}>
-                        <input type="text"/>
-                    </Grid>
-                    <Grid item xs={2}>
-                        <button><img src={Arrow} alt='arrow'/></button>
+                    <Grid container direction='row' className='search-cont'>
+                        <Grid item xs={10}>
+                            <input type="text" placeholder='Enter IP Address...'/>
+                        </Grid>
+                        <Grid item xs={2}>
+                            <button><img src={Arrow} alt='arrow'/></button>
+                        </Grid>
                     </Grid>
                 </Grid>
-                <Grid container direction='row'>
+                <Grid container direction='row' className='info-cont'>
                     <Grid item md={3} xs={12}>
                         <h5>IP ADDRESS</h5>
                         <h3></h3>
